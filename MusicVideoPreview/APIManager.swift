ba684d0eaa0e872eb80cf299ea6a338aff763b45
PaulVagner <prodigyzro@gmail.com>
2016-06-09 13:06:36 -0400

    Part-3 Completed
    Part-3 Coding session completed

--- MusicVideoPreview/APIManager.swift
+++ MusicVideoPreview/APIManager.swift
@@ -22,23 +22,42 @@ class APIManager {
         //takes a string and creates an object
         let url = NSURL(string: urlString)!
         
-        //creates task -> starts out in suspended state
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
-            
-            //runs on background thread
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+        
+        
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
+                }
+                
+            } else {
+                   
+                    //Added for JSONSerialization
+                    //print(data)
                     
-                } else {
-                    completion(result: "NSURLSession successful")
-                    print(data)
+                    do {
+                        /* .AllowFragments - top level object is not Array or Dictionary.
+                        Any type of string or value.   JSONSerialization requires the Do/ Try/ Catch Concerts the NSDATA into a JSON Object and cast it to a Dictionary */
+                        
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                            
+                            print(json)
+                            
+                         let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: "JSONSerialization Successful")
+                                }
+                            }
+                        }
+                    } catch {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completion(result: "error in NSJSONSerialization")
+                        }
+                    }
                 }
             }
+            task.resume()
         }
-        // resumes the task
-        task.resume()
-    
-    }
 }
